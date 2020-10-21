94ba1d9275e8ccff973aa140ba4d5a9496ead226
uditkanotra40 <53949476+uditkanotra40@users.noreply.github.com>
2020-10-21 19:27:39 +0530

    Update maxsubsqauresum.java

--- maxsubsqauresum.java
+++ maxsubsqauresum.java
@@ -1,1 +1,42 @@
 
+public class Solution {
+    public int solve(int[][] A) 
+    {
+        
+        // 000000
+        // for test cases like above
+        int ans=0;
+        for(int i=0;i<A.length;i++)
+        {
+            if(A[i][0]==1);
+            {
+                ans=1;
+                break;
+            }
+        }
+        
+        for(int i=0;i<A[0].length;i++)
+        {
+            if(A[0][i]==1);
+            {
+                ans=1;
+                break;
+            }
+        }
+        
+        
+        
+        for(int i=1;i<A.length;i++)
+        {
+            for(int j=1;j<A[0].length;j++)
+            {
+                if(A[i][j]!=0)
+                {
+                    A[i][j]=Math.min(A[i-1][j-1],Math.min(A[i-1][j],A[i][j-1]))+1;
+                    ans=Math.max(ans,A[i][j]);
+                }
+            }
+        }
+        return ans*ans;
+    }
+}
